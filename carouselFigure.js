f952b0f710d5a32ea10ddc450003c97fc606764c
378840826 <378840826@qq.com>
2017-03-07 13:31:42 +0800

    2次提交，添加高亮等功能
    添加小图片高亮，点击小图切换，居中当前小图等

--- carouselFigure.js
+++ carouselFigure.js
@@ -4,20 +4,12 @@
 3，写出小图 HTML
 4，小图相应高亮
 5，小图坐标位移
+6，小图片点击替换当前大图
 */
 
 // 定义 log 函数
 const log = function() {
     console.log.apply(console, arguments)
-}
-
-//定义 toggleClass
-const toggleClass = function(element, className) {
-    if (element.classList.contains(className)) {
-        element.classList.remove(className)
-    } else {
-        element.classList.add(className)
-    }
 }
 
 //定义 removeClassAll
@@ -92,18 +84,6 @@ const last = function() {
     smallMove()
 }
 
-//绑定下一张按钮事件
-const bindNext = function(event) {
-    var nextButton = document.querySelector('.button_next')
-    nextButton.addEventListener('click', next)
-}
-
-//绑定上一张按钮事件
-const bindLast = function(event) {
-    var lastButton = document.querySelector('.button_last')
-    lastButton.addEventListener('click', last)
-}
-
 //设置当前小图片高亮
 const smallOpacity = function() {
     //找到当前显示的图片
@@ -118,11 +98,11 @@ const smallOpacity = function() {
     nowSmall.classList.add('opacity')
 }
 
-//小图坐标根据显示位移（第3、4张时需要位移）
-//第 3 张时 small_pic 移动 left -130px
-//第 4 张时 small_pic 移动 left -260px
-//第 1 张时 移回 0 点
+//小图坐标根据显示位移
 const smallMove = function() {
+    //第 3 张时 small_pic 移动 left -130px
+    //第 4 张时 small_pic 移动 left -260px
+    //第 1 张时 移回 0 点
     //得到要移动的小图长条容器
     var smallPic = document.querySelector('.small_pic')
     //得到当前显示的图片的下标
@@ -137,19 +117,62 @@ const smallMove = function() {
         smallPic.setAttribute('style', 'left: -260px')
     } else if (nowImgIndex == 0) {
         smallPic.setAttribute('style', 'left: 0px')
+    } else if (nowImgIndex == 1) {
+    //上一张按钮或者点击小图片回退时在 3 转 2 张 时候出现当前图不在中间的情况
+        smallPic.setAttribute('style', 'left: 0px')
     }
-    //上一张按钮时在 3 转 2 张 时候出现高亮图不再中间的 bug
-    if (event.target.classList.contains('button_last')) {
-        if (nowImgIndex == 1) {
-            smallPic.setAttribute('style', 'left: 0px')
-        }
-    }
+}
+
+//小图片点击替换当前大图
+const clickJump = function() {
+    var smallPic = document.querySelector('.small_pic')
+    //得到被点击图片的下标
+    var e = event.target
+    var index = e.id.slice(-1)
+
+    //通过下标替换大图，隐藏当前图片，显示点击的图片
+    var bigPic = document.querySelector('.big_pic')
+    //设置大图容器的 data-active ，用于小图位移监测
+    bigPic.dataset.active = index
+    //当前显示的图片下标(也是id后缀)
+    var activeIndex = parseInt(bigPic.dataset.active)
+    //隐藏当前显示的图片
+    var nowImgId = 'id-big-' + activeIndex
+    var nowImg = document.querySelector(`#${nowImgId}`)
+    removeClassAll('active')
+    //显示点击的图片
+    var newId = 'id-big-' + index
+    var newImgs = document.querySelector(`#${newId}`)
+    newImgs.classList.add('active')
 }
 
 
 
 
+//绑定小图片点击事件
+const bindSmallImgClick = function(event) {
+    var smallImgs = document.querySelectorAll('.smallImg')
+    for (var i = 0; i < smallImgs.length; i++) {
+        var s = smallImgs[i]
+        //循环绑定每一个小图
+        s.addEventListener('click',clickJump)
+        s.addEventListener('click',smallMove)
+        s.addEventListener('click',smallOpacity)
 
+    }
+}
+
+//绑定下一张按钮事件
+const bindNext = function(event) {
+    var nextButton = document.querySelector('.button_next')
+    nextButton.addEventListener('click', next)
+}
+
+//绑定上一张按钮事件
+const bindLast = function(event) {
+    var lastButton = document.querySelector('.button_last')
+    lastButton.addEventListener('click', last)
+}
 
 //绑定所有事件集合
 const bindAll = function() {
@@ -157,6 +180,8 @@ const bindAll = function() {
     bindNext()
     //上一张按钮事件
     bindLast()
+    //小图片点击
+    bindSmallImgClick()
 }
 
 //主函数
